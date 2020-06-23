913c01d8c3b4c49880bece0207d2823de24246e8
Sudhapraba <66582698+Sudhapraba@users.noreply.github.com>
2020-06-23 21:46:10 +0530

    The program to find the area of a triangle

--- areaoftriangle.c
+++ areaoftriangle.c
@@ -10,8 +10,8 @@ scanf("%d%d%d",&a,&b,&c);
 p=(a+b+c);
 s=p/2;
 area=sqrt(s*(s-a)*(s-b)*(s-c));
-printf("Perimeter of a triangle:%.3f\n",p);
-printf("Semiperimter of a triangle:%.3f\n",s);
-printf("Area of a triangle:%.3f",area);
+printf("Perimeter of a triangle:%0.3f\n",p);
+printf("Semiperimter of a triangle:%0.3f\n",s);
+printf("Area of a triangle:%0.3f",area);
 getch();
 }
