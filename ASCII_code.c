aef2d8844dce231b2ef246dec03ed35e9b5cbf5e
Sudhapraba <66582698+Sudhapraba@users.noreply.github.com>
2020-06-25 22:46:57 +0530

    To find the ASCII code for a particular input

---
+++ ASCII_code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char input;
+clrscr();
+printf("Enter the input:");
+input=getchar();
+printf("ASCII code of %c is %d",input,input);
+getch();
+}
