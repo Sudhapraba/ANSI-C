fbd67de5fd9458a5fd0b6244efca116406aab6f0
Sudhapraba <66582698+Sudhapraba@users.noreply.github.com>
2020-08-19 20:24:25 +0530

    The programs displays the 2 × 2 matrix addition

---
+++ matrix_add.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a1[10][10],a2[10][10],s[10][10],i,j,a,b;
+  clrscr();
+  printf("Enter the number of rows:");
+  scanf("%d",&a);
+  printf("Enter the number of columns:");
+  scanf("%d",&b);
+  printf("Enter the elements of first matrix:\n");
+  for(i=0;i<a;i++)
+  {
+    for(j=0;j<b;j++)
+    {
+      scanf("%d",&a1[i][j]);
+    }
+  }
+  printf("Enter the elements of second matrix:\n");
+  for(i=0;i<a;i++)
+  {
+    for(j=0;j<b;j++)
+    {
+      scanf("%d",&a2[i][j]);
+    }
+  }
+  for(i=0;i<a;i++)
+  {
+    for(j=0;j<b;j++)
+    {
+      s[i][j]=a1[i][j]+a2[i][j];
+    }
+  }
+  printf("Addition of two matrices:\n");
+  for(i=0;i<a;i++)
+  {
+    for(j=0;j<b;j++)
+    {
+      printf("%d \t",s[i][j]);
+    }
+    printf("\n");
+  }
+  getch();
+}
